c135d4e81c9c8e6eaf4372a365b3c502a417f85e
MananParmarTR <manan.pravinkumarparmar@thomsonreuters.com>
2024-08-10 12:12:03 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import { SortingVisualizer } from './SortingVisualizer/SortingVisualizer';
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
+    <>
+     <Helmet>
+      <title>SortingVisualizer</title>
+    </Helmet>
     <div className="App">
        <SortingVisualizer></SortingVisualizer>
     </div>
+    </>
   );
 }
 
