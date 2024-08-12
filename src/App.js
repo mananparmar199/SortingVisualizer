4516c9d1182da33cadccaae4d10d3942f844498f
MananParmarTR <manan.pravinkumarparmar@thomsonreuters.com>
2024-08-12 11:36:50 +0530

    falcon icon commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import './App.css';
 import { SortingVisualizer } from './SortingVisualizer/SortingVisualizer';
-import { Helmet } from 'react-helmet';
 
 function App() {
   return (
-    <>
-     <Helmet>
-      <title>SortingVisualizer</title>
-    </Helmet>
     <div className="App">
        <SortingVisualizer></SortingVisualizer>
     </div>
-    </>
   );
 }
 
